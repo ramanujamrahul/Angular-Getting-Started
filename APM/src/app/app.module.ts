5b2635df8907026bbf30c7bd61133600d02ecafb
rahul ramanujam <ramanujamrahul@gmail.com>
2020-07-19 00:19:48 -0400

    sort

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-
+import {FormsModule} from '@angular/forms'
+import { ConvertToSpacesPipe } from './shared/convert-to-spaes.pipe';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector:'pm-products',
-    templateUrl:'./product-list.component.html'
+    templateUrl:'./product-list.component.html',
+    styleUrls:['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
+ngOnInit(): void {
+ console.log("on init");
+}
 pageTitle:string= 'Product List!';
 imageWidth:number=50;
 imageMargin:number=2;
 showImage:boolean=false;
-products: any[]=[
+  private _listFilter: string = 'cart';
+   get listFilter(): string {
+    return this._listFilter;
+  }
+   set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts=this.listFilter?this.performFilter(this.listFilter):this.products;
+  }
+filteredProducts:IProduct[];
+products: IProduct[]=[
     {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -62,6 +76,15 @@ products: any[]=[
         "imageUrl": "assets/images/xbox-controller.png"
       }
 ];
+constructor(){
+  this.filteredProducts=this.products;
+  this.listFilter='cart';
+}
+performFilter(filterBy:string):IProduct[]{
+  filterBy=filterBy.toLocaleLowerCase();
+  return this.products.filter((product:IProduct)=>product
+  .productName.toLocaleLowerCase().indexOf(filterBy)!==-1)
+}
 toggleImage():void{
 this.showImage=!this.showImage;
 }
